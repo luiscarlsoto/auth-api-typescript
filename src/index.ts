e4d85f5d5394edcc1b17d9869c16ddb284578a41
carlos <carlos@queo.com.co>
2023-02-13 23:32:03 -0500

    [ADD] index server

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import express from 'express'
+import { authRoutes } from './routes'
+import 'reflect-metadata'
+import { AppDataSource } from './data-source'
+
+const app = express()
+
+app.use(express.json())
+
+AppDataSource.initialize()
+  .then(() => {
+    console.log('Connected to database')
+  })
+  .catch((error: ErrorCallback) => console.log(error))
+
+app.use('/api/auth', authRoutes)
+
+// app.use('/api/categories', categoriesController)
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000')
+})
