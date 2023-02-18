0c45c49d3091fa6539dcd6f07c846a48aa04bacb
carlos <carlos@queo.com.co>
2023-02-17 22:29:32 -0400

    [ADD]

---
+++ src/types/items.type.ts
@@ -0,0 +1,11 @@
+export interface Items {
+  id: number
+  name: string
+  description: string
+  image: string
+  price: number
+  category_id: number
+  status: number
+  created_at: string
+  updated_at: string
+}
