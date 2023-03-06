81b57aec2df3cc682dd03d65f1abf17eaec955b1
carlos <carlos@queo.com.co>
2023-03-06 02:43:15 -0400

    [EDIT] Normalize api response and add key for i18n

--- src/helpers/validateHelper.helper.ts
+++ src/helpers/validateHelper.helper.ts
@@ -1,17 +1,18 @@
 import { NextFunction, Request, Response } from 'express'
 import { validationResult } from 'express-validator'
-
-interface CustomValidatorMsg {
-  msg: string
-  param: string
-}
+import { respond } from './commons.helper'
 
 export const validate = (req: Request, res: Response, next: NextFunction): void => {
   try {
     validationResult(req).throw()
     return next()
   } catch (error: any) {
-    res.status(403)
-    res.send({ error: error.array().map((error: CustomValidatorMsg) => ({ message: error.msg, param: error.param, code: 403 })) })
+    respond(res, {
+      status: {
+        type: 'error',
+        key: error.array()[0].msg,
+        code: 403
+      }
+    })
   }
 }
