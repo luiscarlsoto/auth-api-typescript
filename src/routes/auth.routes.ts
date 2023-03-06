dedb81425936b2ba2cdb02a21f7d87c2c4d0ccd9
Luis <luiscarlsoto@gmail.com>
2023-03-06 03:14:52 -0400

    [FIX] Removed middleware

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,12 +1,11 @@
 import express from 'express'
 import { signIn, signUp } from '../controllers/auth.controllers'
-import { validateTokenMiddleware } from '../validator/token.validator'
 import { validateUserSignIn, validateUserSignUp } from '../validator/user.validator'
 
 const router = express.Router()
 
 router.post('/signup', validateUserSignUp, signUp as express.RequestHandler)
 
-router.post('/signin', validateUserSignIn, validateTokenMiddleware, signIn as express.RequestHandler)
+router.post('/signin', validateUserSignIn, signIn as express.RequestHandler)
 
 export default router
