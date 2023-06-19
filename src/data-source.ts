43b599f278a9c04516bc5d4409cc544db0d97150
Luis Soto <luiscarlsoto@gmail.com>
2023-06-19 16:24:41 -0400

    [ADD] Task entitie

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,11 +1,12 @@
 import 'reflect-metadata'
 import { DataSource } from 'typeorm'
 import { User } from './entity/user'
+import { Task } from './entity/task'
 import * as dotenv from 'dotenv'
 dotenv.config()
 
 export const AppDataSource = new DataSource({
-  type: 'mysql',
+  type: 'mariadb',
   host: process.env.DB_HOST,
   port: Number(process.env.PORT ?? 3306),
   username: process.env.DB_USER,
@@ -13,7 +14,7 @@ export const AppDataSource = new DataSource({
   database: process.env.DB_NAME,
   synchronize: true,
   logging: false,
-  entities: [User],
+  entities: [User, Task],
   migrationsTableName: 'migrations',
   migrations: [],
   subscribers: []
